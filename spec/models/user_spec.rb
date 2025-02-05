5051f8cae623a585d370e0dce695e3f941aa2d60
Kevin Newland <kevin.newland@yahoo.com>
2025-02-05 16:48:35 -0600

    created specs for each model and tested associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe User, type: :model do
     it { should have_secure_password }
     it { should have_secure_token(:api_key) }
   end
+
+  describe "associations" do
+    it { should belong_to(:host).class_name('User') }
+    it { should have_many(:users_viewing_parties) }
+    it { should have_many(:users).through(:users_viewing_parties) }
+  end
 end
