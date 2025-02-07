5d1631520bef779a0baa4b027f169a9990b7f7bc
Kevin Newland <kevin.newland@yahoo.com>
2025-02-07 10:08:00 -0600

    changes

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -4,6 +4,9 @@ class Api::V1::MoviesController < ApplicationController
       faraday.headers["Authorization"] = Rails.application.credentials.tmdb[:key]
     end
 
-    response = conn.get("")
+    response = conn.get("/3/movie/top_rated", {total_results: num_of_results})
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    
   end
 end
