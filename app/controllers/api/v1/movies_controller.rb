b26f52d41a9b9e30c2f21076e6304933c496cb8e
Kevin Newland <kevin.newland@yahoo.com>
2025-02-07 16:48:52 -0600

    created index action to retrieve top rated movies

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::MoviesController < ApplicationController
 
   def index
-    conn = Faraday.new(url: "https://api.themoviedb.org" ) do |faraday|
-      faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
-    end
-
-    response = conn.get("/3/movie/top_rated", { page: params[:page]})
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    render json: json
+    movies = MovieGateway.get_top_rated
+    render json: MovieSerializer.format_movie_list(movies)
   end
 end
