cd8cc865417239609aac5ee21e1893bb9c8fb857
Kevin Newland <kevin.newland@yahoo.com>
2025-02-07 14:06:53 -0600

    changes

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::MoviesController < ApplicationController
 
   def index
     conn = Faraday.new(url: "https://api.themoviedb.org" ) do |faraday|
-      faraday.headers["Authorization"] = "Bearer #{Rails.application.credentials.tmdb[:key]}"
+      faraday.headers["Authorization"] = Rails.application.credentials.tmdb[:key]
     end
-
+    
     response = conn.get("/3/movie/top_rated", { page: params[:page]})
     json = JSON.parse(response.body, symbolize_names: true)
 
