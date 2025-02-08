7b767280f3a32893006201117ec6ff2cf9e0e88a
Kevin Newland <kevin.newland@yahoo.com>
2025-02-08 12:37:04 -0600

    added functionality to movies controller to retrieve movies based on search params

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::MoviesController < ApplicationController
-
   def index
-    movies = MovieGateway.get_top_rated
+    if params[:search]
+      movies = MovieGateway.get_searched_movies(params[:search])
+    else
+      movies = MovieGateway.get_top_rated
+    end
     render json: MovieSerializer.format_movie_list(movies)
   end
 end
