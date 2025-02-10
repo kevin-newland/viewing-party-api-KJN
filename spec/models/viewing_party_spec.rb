f9c29d323d4519fce68678af04ea560f727e0ed3
Kevin Newland <kevin.newland@yahoo.com>
2025-02-10 17:14:26 -0600

    add test for sad path

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -32,4 +32,13 @@ RSpec.describe ViewingParty, type: :model do
       expect(viewing_party.users.count).to eq(2)
     end
   end
+
+  describe "#end_time_before_start_time"
+    it " returns error if end_time is before start time" do
+      host = User.create!(name: "Danny DeVito", username: "danny_de_v", password: "jerseyMikesRox7")
+
+      viewing_party = ViewingParty.create(host_id: host[:id], name: "Movie Night", start_time: "2025-02-01 20:00:00", end_time: "2025-02-01 18:00:00", movie_id: 101, movie_title: "Inception")
+
+      expect(viewing_party.errors[:end_time]).to match_array("must come after start time")
+    end
 end
